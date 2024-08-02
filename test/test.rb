4c4757a4e89939e4fd5f26c4561475529735e173
SysFugy <149673181+SysFugy@users.noreply.github.com>
2024-08-02 13:22:53 +0000

    Create test.rb

---
+++ test/test.rb
@@ -0,0 +1,11 @@
+require_relative "../color"
+
+puts "I'm white".white # white text.
+puts " I'm red ".red   # red text.
+puts "I'm green".green # green text.
+
+puts "-----------"
+
+puts "I'm white".black_on_white # black text on white background.
+puts " I'm red ".black_on_red   # black text on red background.
+puts "I'm green".black_on_green # black text on green background.
